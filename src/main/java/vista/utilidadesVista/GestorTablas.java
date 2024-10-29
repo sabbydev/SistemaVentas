c435496e167e70421b96aa0bb6ef288473f75d09
Sebastian Alfaro <sebastian.alfaromiranda@gmail.com>
2024-10-29 07:07:23 -0500

    add: utils for view package

---
+++ src/main/java/vista/utilidadesVista/GestorTablas.java
@@ -0,0 +1,79 @@
+package vista.utilidadesVista;
+
+import java.awt.Window;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+public class GestorTablas {
+    
+    public static void deshabilitarEdicion(JTable tabla) {
+        tabla.setDefaultEditor(Object.class, null);
+    }
+    
+    public static void eliminarFilaConConfirmacion(JTable tabla) {
+        int filaSeleccionada = tabla.getSelectedRow();
+        if (filaSeleccionada != -1) {
+            Window parent = SwingUtilities.windowForComponent(tabla);
+
+            int respuesta = JOptionPane.showConfirmDialog(
+                parent,
+                "¿Estás seguro de que deseas eliminar esta fila?",
+                "Confirmar Eliminación",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE
+            );
+
+            if (respuesta == JOptionPane.YES_OPTION) {
+                eliminarFila(tabla, filaSeleccionada);
+            }
+        }
+    }
+    
+    public static void actualizarTabla(JTable tabla, List<Object[]> filas) {
+        vaciarTabla(tabla);
+    
+        for (Object[] fila : filas) {
+            insertarFila(tabla, fila);
+        }
+    }
+
+    public static void vaciarTabla(JTable tabla) {
+        DefaultTableModel modeloTabla = (DefaultTableModel) tabla.getModel();
+        int filas = modeloTabla.getRowCount();
+        for (int i = filas - 1; i >= 0; i--) {
+            modeloTabla.removeRow(i);
+        }
+    }
+
+    public static void insertarFila(JTable tabla, Object[] fila) {
+        DefaultTableModel modeloTabla = (DefaultTableModel) tabla.getModel();
+        modeloTabla.addRow(fila);
+    }
+    
+    public static void eliminarFila(JTable tabla, int fila) {
+        DefaultTableModel modeloTabla = (DefaultTableModel) tabla.getModel();
+        modeloTabla.removeRow(fila);
+    }
+    
+    public static double calcularSumaColumna(JTable tabla, int columna) {
+        double suma = 0.0;
+        
+        TableModel model = tabla.getModel();
+        
+        for (int i = 0; i < model.getRowCount(); i++) {
+            String valorString = model.getValueAt(i, columna).toString();
+            try {
+                double valor = Double.parseDouble(valorString);
+                suma += valor;
+            } catch (NumberFormatException e) {
+                System.out.println("Error al convertir el valor: " + valorString);
+            }
+        }
+        
+        return suma;
+    }
+}
---
+++ src/main/java/vista/utilidadesVista/ValidadorEntradasUsuario.java
@@ -0,0 +1,128 @@
+package vista.utilidadesVista;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class ValidadorEntradasUsuario {
+
+    public static boolean seleccionCbEsValida(JComboBox<String> comboBox, String nombreCampo) {
+        if (comboBox.getSelectedIndex() == 0) {
+            JOptionPane.showMessageDialog(null, "Selecciona una opción en '" + nombreCampo + "'.");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean campoCantidadEsValido(JTextField txtField, String nombreCampo) {
+        if (!tieneContenido(txtField, nombreCampo)) {
+            return false;
+        }
+
+        try {
+            int valorInt = Integer.parseInt(txtField.getText());
+            if (valorInt <= 0) {
+                JOptionPane.showMessageDialog(null, nombreCampo + " debe ser un valor positivo.");
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, nombreCampo + " debe ser un valor tipo numérico entero y positivo.");
+            return false;
+        }
+        
+        return true;
+    }
+
+    public static boolean campoDniRucEsValido(JTextField txtField, String nombreCampo) {
+        String texto = txtField.getText().trim();
+        String regex = "^[0-9]+$";
+
+        if (!tieneContenido(txtField, nombreCampo)) {
+            return false;
+        }
+
+        if (!LongitudDniRucValido(txtField, nombreCampo)) {
+            return false;
+        }
+
+        return cumpleRegex(texto, regex, nombreCampo);
+    }
+
+    public static boolean campoNombrePersonaEsValida(JTextField textField, String nombreCampo) {
+        if (!tieneContenido(textField, nombreCampo)) {
+            return false;
+        }
+
+        String regex = "^[\\p{L} .'-]+$"; // Permite letras, espacios y algunos caracteres especiales
+
+        if (!textField.getText().trim().matches(regex)) {
+            JOptionPane.showMessageDialog(null,
+                "El campo '" + nombreCampo + "' es inválido. Solo se permiten letras, espacios y los caracteres . ' - .",
+                "Error de Validación",
+                JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean campoNombreProductoEsValida(JTextField textField, String nombreCampo) {
+        if (!tieneContenido(textField, nombreCampo)) {
+            return false;
+        }
+
+        String regex = "^[a-zA-Z]+$";
+
+        if (!textField.getText().trim().matches(regex)) {
+            JOptionPane.showMessageDialog(null, "El campo " + nombreCampo + " es inválido. Solo se permiten letras sin tildes.");
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean campoCorreoEsValido(JTextField textField, String nombreCampo) {
+        if (!tieneContenido(textField, nombreCampo)) {
+            return false;
+        }
+
+        String regex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$";
+        return cumpleRegex(textField.getText().trim(), regex, nombreCampo);
+    }
+    
+    public static boolean campoTelefonoEsValido(JTextField textField, String nombreCampo) {
+        if (!tieneContenido(textField, nombreCampo)) {
+            return false;
+        }
+
+        String regex = "^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,4}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$";
+
+        return cumpleRegex(textField.getText().trim(), regex, nombreCampo);
+    }
+    
+    private static boolean LongitudDniRucValido(JTextField textField, String nombreCampo) {
+        int longitud = textField.getText().length();
+
+        if (longitud == 7 || longitud == 11) {
+            return true;
+        } else {
+            JOptionPane.showMessageDialog(null, nombreCampo + " es inválido. El DNI debe tener exactamente 7 dígitos y el RUC debe tener exactamente 11 dígitos.");
+            return false;
+        }
+    }
+    private static boolean tieneContenido(JTextField textField, String nombreCampo) {
+        if (textField.getText().isBlank()) {
+            JOptionPane.showMessageDialog(null, "Completa el campo '" + nombreCampo + "'.");
+            return false;
+        }
+        return true;
+    }
+    private static boolean cumpleRegex(String texto, String regex, String nombreCampo) {
+        if (!texto.matches(regex)) {
+            JOptionPane.showMessageDialog(null, "El campo " + nombreCampo + " es inválido. Solo se permiten dígitos.");
+            return false;
+        }
+        
+        return true;
+    }
+}
